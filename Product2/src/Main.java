0bf33662b0c1702d8ec0c3ef05dba6af8eb99096
Kamiotoshi <133650059+Kamiotoshi@users.noreply.github.com>
2023-11-16 11:28:20 +0700

    Add files via upload

---
+++ Product2/src/Main.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Tạo 3 sản phẩm
+        Product product1 = new Product("SP001", "Iphone", 1200.0, 10, "USD");
+        Product product2 = new Product("SP002", "Samsung", 500.0, 20, "USD");
+        Product product3 = new Product("SP003", "Laptop", 20.0, 50, "VND");
+
+        // In thông tin sản phẩm
+        product1.Show();
+        product2.Show();
+        product3.Show();
+
+        // Thực hiện một số thay đổi và in lại thông tin
+        product1.Increasequantity(5);
+        product2.Lowerquantity(15);
+        product3.Pricechanger(25.0);
+
+        // In lại thông tin sau khi thay đổi
+        product1.Show();
+        product2.Show();
+        product3.Show();
+
+        // Kiểm tra tình trạng hàng
+        product1.Check();
+        product2.Check();
+        product3.Check();
+    }
+}
---
+++ Product2/src/Product.java
@@ -0,0 +1,57 @@
+public class Product {
+    // Các thuộc tính
+    private String Productnumber;
+    private String Productname;
+    private double Price;
+    private int quantity;
+    private String unit;
+
+    // Constructor
+    public Product(String Productnumber, String Productname, double Price, int quantity, String unit) {
+        this.Productnumber = Productnumber;
+        this.Productname = Productname;
+        this.Price = Price;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    // Phương thức tăng số lượng sản phẩm
+    public void Increasequantity(int quantityUp) {
+        this.quantity += quantityUp;
+    }
+
+    // Phương thức giảm số lượng sản phẩm
+    public void Lowerquantity(int quantityDown) {
+        if (quantityDown > this.quantity) {
+            System.out.println("Số lượng giảm lớn hơn số lượng hiện có.");
+        } else {
+            this.quantity -= quantityDown;
+        }
+    }
+
+    // Phương thức thay đổi giá tiền sản phẩm
+    public void Pricechanger(double NewPrice) {
+        this.Price = NewPrice;
+    }
+
+    // Phương thức kiểm tra hàng còn/hết hàng
+    public void Check() {
+        if (quantity > 0) {
+            System.out.println("Còn hàng. Số lượng hiện tại: " + quantity);
+        } else {
+            System.out.println("Hết hàng.");
+        }
+    }
+
+    // Phương thức in thông tin sản phẩm
+    public void Show() {
+        System.out.println("Mã sản phẩm: " + Productnumber);
+        System.out.println("Tên sản phẩm: " + Productname);
+        System.out.println("Giá tiền: " + Price + " " + unit);
+        System.out.println("Số lượng: " + quantity);
+        System.out.println("Đơn vị: " + unit);
+        System.out.println("------------------------------");
+    }
+
+
+}
